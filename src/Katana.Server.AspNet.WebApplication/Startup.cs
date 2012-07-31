9f60c14a689e7fa7db94f0c1d9f39c513118b24c
Louis DeJardin <louis.dejardin@gmail.com>
2012-07-31 16:26:02 -0700

    Adding small Startup-style class to sln app

---
+++ src/Katana.Server.AspNet.WebApplication/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Owin;
+using Gate;
+using Gate.Middleware;
+using Katana.WebApi;
+
+namespace Katana.Server.AspNet.WebApplication
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder builder)
+        {
+            builder
+                .UseShowExceptions()
+                .UseMessageHandler<TraceRequestFilter>()
+                .Run(Wilson.App());
+        }
+    }
+}
