f8e1bcd313abbedf17214937dc51c3e89f8a26e9
Louis DeJardin <louis.dejardin@gmail.com>
2012-09-18 17:33:45 -0700

    Removing unused constants

--- src/Microsoft.AspNet.Owin/Constants.cs
+++ src/Microsoft.AspNet.Owin/Constants.cs
@@ -11,44 +11,15 @@ namespace Microsoft.AspNet.Owin
     /// </summary>
     internal static class Constants
     {
-        public const string VersionKey = "owin.Version";
-        public const string OwinVersion = "1.0";
-
         public const string ServerNameKey = "server.Name";
         public static readonly string ServerName = "ASP.NET 4.0, Microsoft.AspNet.Owin " + typeof(Constants).Assembly.GetName().Version.ToString();
         public const string ServerVersionKey = "msaspnet.AdapterVersion";
         public static readonly string ServerVersion = typeof(Constants).Assembly.GetName().Version.ToString();
-
-        public const string RequestSchemeKey = "owin.RequestScheme";
-        public const string RequestMethodKey = "owin.RequestMethod";
-        public const string RequestPathBaseKey = "owin.RequestPathBase";
-        public const string RequestPathKey = "owin.RequestPath";
-        public const string RequestQueryStringKey = "owin.RequestQueryString";
-        public const string HttpRequestProtocolKey = "owin.RequestProtocol";
-        public const string HttpResponseProtocolKey = "owin.ResponseProtocol";
-        public const string ReasonPhraseKey = "owin.ReasonPhrase";
-        public const string CallCompletedKey = "owin.CallCompleted";
-
-        public const string ClientCertifiateKey = "ssl.ClientCertificate";
-
-        public const string RemoteEndPointKey = "host.RemoteEndPoint";
-        public const string LocalEndPointKey = "host.LocalEndPoint";
-        public const string IsLocalKey = "host.IsLocal";
-
-        public const string WebSocketSupportKey = "websocket.Support";
-        public const string WebSocketAcceptKey = "websocket.Accept";
-        public const string WebSocketSupport = "WebSocketFunc";
 
         public const string SendFileVersionKey = "sendfile.Version";
         public const string SendFileVersion = "1.0";
         public const string SendFileSupportKey = "sendfile.Support";
         public const string SendFileSupport = "SendFileFunc";
         public const string SendFileFuncKey = "sendfile.Func";
-
-        public const string WwwAuthenticateHeader = "WWW-Authenticate";
-        public const string ContentLengthHeader = "Content-Length";
-        public const string TransferEncodingHeader = "Transfer-Encoding";
-        public const string KeepAliveHeader = "Keep-Alive";
-        public const string ConnectionHeader = "Connection";
     }
 }
