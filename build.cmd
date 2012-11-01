acf64b596f2a0bc08b11459d34acab2a05989d2b
Louis DeJardin <louis.dejardin@gmail.com>
2012-11-01 14:13:04 -0700

    Moving Sakefile to build folder

--- build.cmd
+++ build.cmd
@@ -2,7 +2,7 @@
 cd %~dp0
 
 set EnableNuGetPackageRestore=true
-".nuget\NuGet.exe" install Sake -pre -o packages
-for /f "tokens=*" %%G in ('dir /AD /ON /B "packages\Sake.*"') do set __sake__=%%G
-"packages\%__sake__%\tools\Sake.exe" -I src/build -f Sakefile.shade %*
-set __sake__=
+
+.nuget\NuGet.exe install Sake -version 0.1.4 -o packages
+
+packages\Sake.0.1.4\tools\Sake.exe -I build -f Sakefile.shade %*
