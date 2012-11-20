05cfdbf65e5f7152597aca1a43c721d3c97cff50
Louis DeJardin <louis.dejardin@gmail.com>
2012-11-19 22:55:27 -0800

    Adding integration tests project

---
+++ tests/Microsoft.Owin.Host.IntegrationTests/SimpleGetTests.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Owin;
+using Shouldly;
+using Xunit.Extensions;
+
+namespace Microsoft.Owin.Host.IntegrationTests
+{
+    public class SimpleGetTests : TestBase
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseFunc(_ => Invoke);
+        }
+
+        public Task Invoke(IDictionary<string, object> env)
+        {
+            var headers = (IDictionary<string, string[]>)env["owin.ResponseHeaders"];
+            headers["Content-Type"] = new string[] { "text/plain" };
+            var body = (Stream)env["owin.ResponseBody"];
+            using (var writer = new StreamWriter(body))
+            {
+                writer.Write("<p>alpha</p>");
+            }
+            return TaskHelpers.Completed();
+        }
+
+        [Theory]
+        [InlineData("Microsoft.Owin.Host.SystemWeb")]
+        [InlineData("Microsoft.Owin.Host.HttpListener")]
+        public void ResponseBodyShouldArrive(string serverName)
+        {
+            var port = base.RunWebServer(
+                serverName,
+                typeof(SimpleGetTests).FullName);
+
+            var client = new HttpClient();
+            client.GetStringAsync("http://localhost:" + port + "/text")
+                .Then(body => body.ShouldBe("<p>alpha</p>"));
+        }
+
+
+        [Theory]
+        [InlineData("Microsoft.Owin.Host.SystemWeb")]
+        [InlineData("Microsoft.Owin.Host.HttpListener")]
+        public void ContentTypeShouldBeSet(string serverName)
+        {
+            var port = base.RunWebServer(
+                serverName,
+                typeof(SimpleGetTests).FullName);
+
+            var client = new HttpClient();
+            client.GetAsync("http://localhost:" + port + "/text")
+                .Then(message => message.Content.Headers.ContentType.MediaType.ShouldBe("text/html"));
+        }
+
+    }
+}
---
+++ tests/Microsoft.Owin.Host.IntegrationTests/TestBase.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Katana.Engine;
+
+namespace Microsoft.Owin.Host.IntegrationTests
+{
+    public class TestBase : IDisposable
+    {
+        readonly CancellationTokenSource _disposing = new CancellationTokenSource();
+
+        public void Dispose()
+        {
+            _disposing.Cancel(false);
+        }
+
+        public int RunWebServer(
+            string serverName = null,
+            string applicationName = null)
+        {
+            if (serverName == "Microsoft.Owin.Host.SystemWeb")
+            {
+                return RunWebServerSystemWeb(applicationName);
+            }
+            else
+            {
+                return RunWebServerViaEngine(serverName, applicationName);
+            }
+        }
+
+        private int RunWebServerViaEngine(
+            string serverName,
+            string applicationName)
+        {
+            var port = 8080;
+
+            var sourceDirectory = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+
+            var targetDirectory = BuildTargetDirectory(
+                sourceDirectory,
+                applicationName,
+                port);
+
+            Directory.SetCurrentDirectory(targetDirectory);
+
+            var server = WebApplication.Start(
+                boot: "Default",
+                server: serverName,
+                app: applicationName,
+                port: port);
+
+            _disposing.Token.Register(() =>
+            {
+                server.Dispose();
+                try
+                {
+                    Directory.Delete(targetDirectory, true);
+                }
+                catch(Exception ex)
+                {
+                    Trace.WriteLine(string.Format("Cleanup error {0}", ex.Message));
+                }
+            });
+
+            return port;
+        }
+
+        private int RunWebServerSystemWeb(string applicationName)
+        {
+            var tcs = new TaskCompletionSource<object>();
+
+            var port = GetAvailablePort();
+
+            var sourceDirectory = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+
+            var targetDirectory = BuildTargetDirectory(
+                sourceDirectory,
+                applicationName,
+                port);
+
+            var targetHostConfig = Path.Combine(targetDirectory, "ApplicationHost.config");
+
+            string programFile32 = Environment.GetEnvironmentVariable("ProgramFiles(x86)");
+            string iisExpressExe = Path.Combine(programFile32, "IIS Express", "iisexpress.exe");
+
+
+            var info = new ProcessStartInfo
+            {
+                WorkingDirectory = targetDirectory,
+                UseShellExecute = false,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                ErrorDialog = false,
+                CreateNoWindow = false,
+                FileName = iisExpressExe,
+                Arguments = "/config:\"" + targetHostConfig + "\" /trace:error",
+            };
+
+            //Log.Debug("Executing {0}", Definition.Command);
+            Process process = Process.Start(info);
+
+            process.OutputDataReceived += OutputDataReceived;
+            process.ErrorDataReceived += OutputDataReceived;
+            process.Exited += (a, b) => tcs.TrySetResult(null);
+            process.EnableRaisingEvents = true;
+
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+
+            _disposing.Token.Register(() =>
+            {
+                //process.CloseMainWindow();
+                //process.StandardInput.WriteLine("Q");
+                //process.StandardInput.Flush();
+                //process.StandardInput.Close();
+                //tcs.Task.Wait(250);
+                if (!process.HasExited)
+                {
+                    SetForegroundWindow(process.MainWindowHandle);
+                    SendKeys.SendWait("Q");
+                    tcs.Task.Wait(1000);
+                }
+                if (!process.HasExited)
+                {
+                    process.Kill();
+                    tcs.Task.Wait();
+                }
+                try
+                {
+                    Directory.Delete(targetDirectory, true);
+                }
+                catch (Exception ex)
+                {
+                    Trace.WriteLine(string.Format("Cleanup error {0}", ex.Message));
+                }
+            });
+
+            return port;
+        }
+
+        private static string BuildTargetDirectory(
+            string workingDirectory,
+            string applicationName,
+            int port)
+        {
+            string targetDirectory = Path.Combine(workingDirectory, "Port" + port + "-" + Guid.NewGuid().ToString("n"));
+            string binDirectory = Path.Combine(targetDirectory, "bin");
+
+            string sourceHostConfig = Path.Combine(workingDirectory, "applicationHost.config");
+            var targetHostConfig = Path.Combine(targetDirectory, "applicationHost.config");
+
+            string sourceWebConfig = Path.Combine(workingDirectory, "web.config");
+            string targetWebConfig = Path.Combine(targetDirectory, "web.config");
+
+            Directory.CreateDirectory(targetDirectory);
+            Directory.CreateDirectory(binDirectory);
+
+            File.WriteAllText(
+                targetHostConfig,
+                File.ReadAllText(sourceHostConfig)
+                    .Replace("TheBindingInformation", ":" + port + ":localhost")
+                    .Replace("ThePhysicalPath", targetDirectory));
+
+            File.WriteAllText(
+                targetWebConfig,
+                File.ReadAllText(sourceWebConfig)
+                    .Replace("TheApplicationName", applicationName));
+
+            foreach (var assemblyName in Directory.GetFiles(workingDirectory, "*.dll"))
+            {
+                File.Copy(
+                    Path.Combine(workingDirectory, assemblyName),
+                    Path.Combine(binDirectory, Path.GetFileName(assemblyName)),
+                    overwrite: false);
+            }
+            return targetDirectory;
+        }
+
+        private static int GetAvailablePort()
+        {
+            var socket = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.IP);
+
+            socket.Bind(new IPEndPoint(IPAddress.Any, 0));
+            var ipEndPoint = socket.LocalEndPoint as IPEndPoint;
+            int port = 0;
+            if (ipEndPoint != null)
+            {
+                port = ipEndPoint.Port;
+            }
+            socket.Close();
+            return port;
+        }
+
+        // Activate an application window.
+        [DllImport("USER32.DLL")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        private void OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            Trace.WriteLine(e.Data);
+        }
+
+    }
+}
