1998287ca6edfd55fdd97fd304d6cae196e8b8f9
Tratcher <chrross@CHRROSS-DEV.redmond.corp.microsoft.com>
2012-11-20 15:18:57 -0800

    Selfhost perf project for F5.

---
+++ tests/Katana.Performance.ReferenceApp.Selfhost/Program.cs
@@ -0,0 +1,19 @@
+﻿using Katana.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katana.Performance.ReferenceApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new KatanaStarter().Start(new StartParameters() { App = "Katana.Performance.ReferenceApp.Startup" });
+            Console.WriteLine("Started");
+            Console.ReadKey();
+        }
+    }
+}
