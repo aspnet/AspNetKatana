f09383c37348d46b09836e071d308b6d119a76e9
Louis DeJardin <louis.dejardin@gmail.com>
2012-11-28 12:48:26 -0800

    Eliminating a few more stylecop warnings

--- src/Tools/CertificateInstaller/CertInstaller/Program.cs
+++ src/Tools/CertificateInstaller/CertInstaller/Program.cs
@@ -1,21 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Security.Cryptography.X509Certificates;
+﻿// <copyright file="Program.cs" company="Katana contributors">
+//   Copyright 2011-2012 Katana contributors
+// </copyright>
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
 using System.Diagnostics;
 
 namespace CertInstaller
 {
     internal enum InstallerCommand
-    { 
+    {
         Install,
         Uninstall
     }
 
-    class Program
+    internal class Program
     {
-        static int Main(string[] args)
-        {     
+        private static int Main(string[] args)
+        {
             if (args.Length != 3)
             {
                 PrintUsage();
@@ -28,7 +41,7 @@ namespace CertInstaller
                 string certName = args[1];
                 string certPassword = args[2];
 
-                CertificateInstaller installer = new CertificateInstaller(certName, certPassword);
+                var installer = new CertificateInstaller(certName, certPassword);
 
                 switch (command)
                 {
@@ -61,7 +74,7 @@ namespace CertInstaller
             }
         }
 
-        private static InstallerCommand GetCommand (string command)
+        private static InstallerCommand GetCommand(string command)
         {
             if (string.IsNullOrEmpty(command))
             {
--- src/Tools/CertificateInstaller/CertInstaller/Properties/AssemblyInfo.cs
+++ src/Tools/CertificateInstaller/CertInstaller/Properties/AssemblyInfo.cs
@@ -1,10 +1,26 @@
+// <copyright file="AssemblyInfo.cs" company="Katana contributors">
+//   Copyright 2011-2012 Katana contributors
+// </copyright>
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+
 [assembly: AssemblyTitle("CertInstaller")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -17,9 +33,11 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
+
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
+
 [assembly: Guid("8831c1a9-e3b9-4ee6-9c3e-129c93e8550b")]
 
 // Version information for an assembly consists of the following four values:
@@ -32,5 +50,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("0.10")]
+
 [assembly: AssemblyVersion("0.10")]
-[assembly: AssemblyFileVersion("0.10.21119.0")]
+[assembly: AssemblyFileVersion("0.10.21127.0")]
