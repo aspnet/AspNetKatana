ab07bfaa843373100cc5d442e31b2803b9414cd1
Louis DeJardin <louis.dejardin@gmail.com>
2012-12-10 16:40:52 -0800

    Bumping version number

--- tests/Katana.Performance.ReferenceApp.Selfhost/Properties/AssemblyInfo.cs
+++ tests/Katana.Performance.ReferenceApp.Selfhost/Properties/AssemblyInfo.cs
@@ -37,6 +37,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.15")]
-[assembly: AssemblyVersion("0.15")]
-[assembly: AssemblyFileVersion("0.15.21210.0")]
+// [assembly: AssemblyVersion("0.16")]
+[assembly: AssemblyVersion("0.16")]
+[assembly: AssemblyFileVersion("0.16.21210.0")]
