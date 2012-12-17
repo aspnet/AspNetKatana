e21999665f86536abd16f2ab2996f6bea2c7ebe8
Louis DeJardin <louis.dejardin@gmail.com>
2012-12-17 11:45:42 -0800

    Unit testing content type provider
    Also commenting out unwanted default static file types (*.config)

---
+++ tests/Microsoft.Owin.StaticFiles.Tests/DefaultContentTypeProviderTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Owin.StaticFiles.ContentTypes;
+using Shouldly;
+using Xunit;
+
+namespace Microsoft.Owin.StaticFiles.Tests
+{
+    public class DefaultContentTypeProviderTests
+    {
+        [Fact]
+        public void UnknownExtensionsReturnFalse()
+        {
+            var provider = new DefaultContentTypeProvider();
+            string contentType;
+            provider.TryGetContentType("unknown.ext", out contentType).ShouldBe(false);
+        }
+        [Fact]
+        public void KnownExtensionsReturnTrye()
+        {
+            var provider = new DefaultContentTypeProvider();
+            string contentType;
+            provider.TryGetContentType("known.txt", out contentType).ShouldBe(true);
+            contentType.ShouldBe("text/plain");
+        }
+        [Fact]
+        public void DoubleDottedExtensionsAreNotSupported()
+        {
+            var provider = new DefaultContentTypeProvider();
+            string contentType;
+            provider.TryGetContentType("known.exe.config", out contentType).ShouldBe(false);
+        }
+        [Fact]
+        public void DashedExtensionsShouldBeMatched()
+        {
+            var provider = new DefaultContentTypeProvider();
+            string contentType;
+            provider.TryGetContentType("known.dvr-ms", out contentType).ShouldBe(true);
+            contentType.ShouldBe("video/x-ms-dvr");
+        }
+        [Fact]
+        public void BothSlashFormatsAreUnderstood()
+        {
+            var provider = new DefaultContentTypeProvider();
+            string contentType;
+            provider.TryGetContentType(@"/first/example.txt", out contentType).ShouldBe(true);
+            contentType.ShouldBe("text/plain");
+            provider.TryGetContentType(@"\second\example.txt", out contentType).ShouldBe(true);
+            contentType.ShouldBe("text/plain");
+        }
+        [Fact]
+        public void DotsInDirectoryAreIgnored()
+        {
+            var provider = new DefaultContentTypeProvider();
+            string contentType;
+            provider.TryGetContentType(@"/first.css/example.txt", out contentType).ShouldBe(true);
+            contentType.ShouldBe("text/plain");
+            provider.TryGetContentType(@"\second.css\example.txt", out contentType).ShouldBe(true);
+            contentType.ShouldBe("text/plain");
+        }
+    }
+}
