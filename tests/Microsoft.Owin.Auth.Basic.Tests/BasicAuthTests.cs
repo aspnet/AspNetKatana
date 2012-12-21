3d2e457f4c6d3ca9fee59af677c941cc32fbd4cc
Tratcher <chrross@CHRROSS-DEV.redmond.corp.microsoft.com>
2012-12-21 15:19:20 -0800

    Basic Auth tests

--- tests/Microsoft.Owin.Auth.Basic.Tests/BasicAuthTests.cs
+++ tests/Microsoft.Owin.Auth.Basic.Tests/BasicAuthTests.cs
@@ -16,6 +16,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -26,12 +27,66 @@ namespace Microsoft.Owin.Auth.Basic.Tests
     public class BasicAuthTests
     {
         private static readonly AppFunc NotImplemented = env => { throw new NotImplementedException(); };
+        private static readonly AppFunc Success = env => { env["owin.ResponseStatusCode"] = 200; return null; };
 
         [Fact]
         public void Ctor_NullParameters_Throws()
         {
             Assert.Throws<ArgumentNullException>(() => new BasicAuthMiddleware(null, new BasicAuthMiddleware.Options()));
             Assert.Throws<ArgumentNullException>(() => new BasicAuthMiddleware(NotImplemented, null));
+        }
+
+        [Fact]
+        public void NoAuthHeader_PassesThrough()
+        {
+            IDictionary<string, object> environment = CreateEmptyRequest();
+            BasicAuthMiddleware auth = new BasicAuthMiddleware(Success, new BasicAuthMiddleware.Options());
+            auth.Invoke(environment);
+
+            Assert.Equal(200, environment["owin.ResponseStatusCode"]);
+        }
+
+        [Fact]
+        public void BasicAuthHeader_ParsedAndDelegateInvoked()
+        {
+            string user = "user";
+            string psw = "pwsd";
+            string header = "basic: " + Convert.ToBase64String(Encoding.ASCII.GetBytes(user + ":" + psw));
+            IDictionary<string, object> environment = CreateEmptyRequest(header);
+
+            BasicAuthMiddleware.Options options = new BasicAuthMiddleware.Options();
+            options.Authenticate = (env, usr, pass) =>
+            {
+                Assert.Equal(environment, env);
+                Assert.Equal(user, usr);
+                Assert.Equal(psw, pass);
+                return CompletedTask(true);
+            };
+            BasicAuthMiddleware auth = new BasicAuthMiddleware(Success, options);
+            auth.Invoke(environment);
+
+            Assert.Equal(200, environment["owin.ResponseStatusCode"]);
+        }
+
+        private Task<bool> CompletedTask(bool result)
+        {
+            TaskCompletionSource<bool> tcs = new TaskCompletionSource<bool>();
+            tcs.TrySetResult(result);
+            return tcs.Task;
+        }
+
+        private IDictionary<string, object> CreateEmptyRequest(string authHeader = null)
+        {
+            IDictionary<string, object> environment = new Dictionary<string, object>();
+            Dictionary<string, string[]> requestHeaders = new Dictionary<string, string[]>(StringComparer.OrdinalIgnoreCase);
+            environment["owin.RequestHeaders"] = requestHeaders;
+            if (authHeader != null)
+            {
+                requestHeaders["Authorization"] = new string[] { authHeader };
+            }
+
+            environment["server.OnSendingHeaders"] = new Action<Action<object>, object>((a, b) => { });
+            return environment;
         }
     }
 }
