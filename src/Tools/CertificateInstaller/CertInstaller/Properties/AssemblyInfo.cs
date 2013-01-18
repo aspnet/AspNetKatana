ae810309f4efae48e9a8d367a5c584757dea922e
Louis DeJardin <louis.dejardin@gmail.com>
2013-01-18 12:27:25 -0800

    Version attributes come from DevAssemblyInfo.cs link

--- src/Tools/CertificateInstaller/CertInstaller/Properties/AssemblyInfo.cs
+++ src/Tools/CertificateInstaller/CertInstaller/Properties/AssemblyInfo.cs
@@ -24,10 +24,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("CertInstaller")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft Corporation")]
-[assembly: AssemblyProduct("CertInstaller")]
-[assembly: AssemblyCopyright("(c) 2011-2012 Microsoft Corporation All rights reserved.")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -39,16 +35,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly: Guid("8831c1a9-e3b9-4ee6-9c3e-129c93e8550b")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("0")]
-[assembly: AssemblyVersion("0")]
-[assembly: AssemblyFileVersion("0.19.20117.0")]
--- tests/Katana.Performance.ReferenceApp.Selfhost/Properties/AssemblyInfo.cs
+++ tests/Katana.Performance.ReferenceApp.Selfhost/Properties/AssemblyInfo.cs
@@ -27,16 +27,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("9ae7253e-3aef-425b-bff8-83e4b5a7a44b")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("0")]
-[assembly: AssemblyVersion("0")]
-[assembly: AssemblyFileVersion("0.19.20117.0")]
