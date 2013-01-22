9d330a80dd1da41cc62ed5c90e92a9943f7164cd
Tratcher <Tratcher@hotmail.com>
2013-01-22 15:08:39 -0800

    Hosting: CodeAnalysis justifications.

--- tests/Microsoft.Owin.Hosting.Tests/GlobalSuppressions.cs
+++ tests/Microsoft.Owin.Hosting.Tests/GlobalSuppressions.cs
@@ -8,12 +8,12 @@
 // "In Suppression File".
 // You do not need to add suppressions to this file manually.
 
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1063:ImplementIDisposableCorrectly", Scope = "type", Target = "TinyIoC.SafeDictionary`2")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1063:ImplementIDisposableCorrectly", Scope = "member", Target = "TinyIoC.SafeDictionary`2.#Dispose()")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope = "type", Target = "TinyIoC.TinyIoCResolutionException")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope = "type", Target = "TinyIoC.TinyIoCRegistrationTypeException")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope = "type", Target = "TinyIoC.TinyIoCRegistrationException")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope = "type", Target = "TinyIoC.TinyIoCWeakReferenceException")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope = "type", Target = "TinyIoC.TinyIoCConstructorResolutionException")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope = "type", Target = "TinyIoC.TinyIoCAutoRegistrationException")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope = "type", Target = "TinyIoC.NamedParameterOverloads")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1063:ImplementIDisposableCorrectly", Scope = "type", Target = "TinyIoC.SafeDictionary`2", Justification = "Dependency as source")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1063:ImplementIDisposableCorrectly", Scope = "member", Target = "TinyIoC.SafeDictionary`2.#Dispose()", Justification = "Dependency as source")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope = "type", Target = "TinyIoC.TinyIoCResolutionException", Justification = "Dependency as source")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope = "type", Target = "TinyIoC.TinyIoCRegistrationTypeException", Justification = "Dependency as source")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope = "type", Target = "TinyIoC.TinyIoCRegistrationException", Justification = "Dependency as source")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope = "type", Target = "TinyIoC.TinyIoCWeakReferenceException", Justification = "Dependency as source")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope = "type", Target = "TinyIoC.TinyIoCConstructorResolutionException", Justification = "Dependency as source")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope = "type", Target = "TinyIoC.TinyIoCAutoRegistrationException", Justification = "Dependency as source")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope = "type", Target = "TinyIoC.NamedParameterOverloads", Justification = "Dependency as source")]
