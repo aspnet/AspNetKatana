5faba39890e1d42c7127175a557c37b60d768cc7
Louis DeJardin <louis.dejardin@gmail.com>
2013-02-28 15:16:35 -0800

    Experimenting with a Roslyn app loader

---
+++ src/Katana.Loader.Roslyn/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿// <copyright file="AssemblyInfo.cs" company="Microsoft Open Technologies, Inc.">
+// Copyright 2011-2013 Microsoft Open Technologies, Inc. All rights reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("Katana.Loader.Roslyn")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("0430dd70-f08d-4a57-9911-3afcdd79f414")]
---
+++ src/Katana.Loader.Roslyn/RoslynAppLoader.cs
@@ -0,0 +1,54 @@
+﻿// <copyright file="RoslynAppLoader.cs" company="Microsoft Open Technologies, Inc.">
+// Copyright 2011-2013 Microsoft Open Technologies, Inc. All rights reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System;
+using System.IO;
+using Microsoft.Owin.Hosting.Loader;
+using Owin;
+using Roslyn.Scripting;
+using Roslyn.Scripting.CSharp;
+
+namespace Katana.Loader.Roslyn
+{
+    public class RoslynAppLoaderFactory : IAppLoaderFactory
+    {
+        public int Order
+        {
+            get { return -50; }
+        }
+
+        public Func<string, Action<IAppBuilder>> CreateAppLoader(Func<string, Action<IAppBuilder>> nextLoader)
+        {
+            return name =>
+            {
+                string extension = Path.GetExtension(name);
+
+                if (string.Equals(".csx", extension, StringComparison.OrdinalIgnoreCase) && File.Exists(name))
+                {
+                    return app =>
+                    {
+                        var engine = new ScriptEngine();
+                        Session session = engine.CreateSession(new HostObject(app));
+                        session.AddReference(typeof(IAppBuilder).Assembly);
+                        session.AddReference(typeof(HostObject).Assembly);
+                        session.ExecuteFile(name);
+                    };
+                }
+                return nextLoader(name);
+            };
+        }
+    }
+}
