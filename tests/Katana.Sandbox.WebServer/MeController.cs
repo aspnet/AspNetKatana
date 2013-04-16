ffcd77493f56eb99c221ca4973794d731edc6952
Chris Ross <Tratcher@Outlook.com>
2013-04-16 09:15:59 -0700

    Stylecop.

--- tests/Katana.Sandbox.WebServer/MeController.cs
+++ tests/Katana.Sandbox.WebServer/MeController.cs
@@ -1,4 +1,20 @@
-﻿using System.Collections.Generic;
+﻿// <copyright file="MeController.cs" company="Microsoft Open Technologies, Inc.">
+// Copyright 2011-2013 Microsoft Open Technologies, Inc. All rights reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Formatting;
@@ -8,17 +24,6 @@ using System.Web.Http;
 
 namespace Katana.Sandbox.WebServer
 {
-    public class Me
-    {
-        public List<Detail> Details { get; set; }
-    }
-
-    public class Detail
-    {
-        public string Name { get; set; }
-        public string Value { get; set; }
-    }
-
     public class MeController : ApiController
     {
         public HttpResponseMessage Get()
@@ -37,6 +42,17 @@ namespace Katana.Sandbox.WebServer
                     },
                     new JsonMediaTypeFormatter())
             };
+        }
+
+        public class Me
+        {
+            public List<Detail> Details { get; set; }
+        }
+
+        public class Detail
+        {
+            public string Name { get; set; }
+            public string Value { get; set; }
         }
     }
 }
--- tests/Katana.Sandbox.WebServer/Startup.cs
+++ tests/Katana.Sandbox.WebServer/Startup.cs
@@ -1,4 +1,20 @@
-﻿using System.Collections.Generic;
+﻿// <copyright file="Startup.cs" company="Microsoft Open Technologies, Inc.">
+// Copyright 2011-2013 Microsoft Open Technologies, Inc. All rights reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System.Collections.Generic;
 using System.IO;
 using System.Security.Claims;
 using System.Text;
@@ -42,7 +58,6 @@ namespace Katana.Sandbox.WebServer
                 {
                     OnValidateIdentity = async context =>
                     {
-
                     }
                 }
             });
@@ -87,16 +102,6 @@ namespace Katana.Sandbox.WebServer
             var config = new HttpConfiguration();
             config.Routes.MapHttpRoute("Default", "api/{controller}");
             app.UseWebApi(config);
-        }
-    }
-
-    static class MoreExtensions
-    {
-        public static async Task<ClaimsPrincipal> Authenticate(this OwinRequest request, params string[] authenticationTypes)
-        {
-            var identities = new List<ClaimsIdentity>();
-            await request.Authenticate(authenticationTypes, identity => identities.Add(new ClaimsIdentity(identity)));
-            return identities.Count != 0 ? new ClaimsPrincipal(identities) : null;
         }
     }
 }
