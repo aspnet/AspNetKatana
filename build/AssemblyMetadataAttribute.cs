92d16b09e29f800ef85c20c3a2c6bf3fe0949752
Chris Ross <Tratcher@Outlook.com>
2013-05-20 13:54:08 -0700

    Build: Add [assembly: AssemblyMetadata("Servicing", "true")] for shipping assemblies.

---
+++ build/AssemblyMetadataAttribute.cs
@@ -0,0 +1,39 @@
+// <copyright file="AssemblyMetaDataAttribute.cs" company="Microsoft Open Technologies, Inc.">
+// Copyright 2011-2013 Microsoft Open Technologies, Inc. All rights reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+#if NET40
+
+namespace System.Reflection
+{
+    /// <summary>
+    /// Provided as a down-level stub for the 4.5 AssemblyMetaDataAttribute class.
+    /// All released assemblies should define [AssemblyMetadata(“Servicing”,”true”)].
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true, Inherited = false)]
+    internal sealed class AssemblyMetadataAttribute : Attribute
+    {
+        public AssemblyMetadataAttribute(string key, string value)
+        {
+            Key = key;
+            Value = value;
+        }
+
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
+
+#endif
--- build/CommonAssemblyInfo.cs
+++ build/CommonAssemblyInfo.cs
@@ -23,6 +23,8 @@ using System.Reflection;
 [assembly: AssemblyVersion("1.1.0.0")]
 [assembly: AssemblyFileVersion("1.1.20501.0")]
 [assembly: AssemblyInformationalVersion("1.1.0-beta1-20501-000")]
+[assembly: AssemblyMetadata("Servicing", "true")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1016:MarkAssembliesWithAssemblyVersion", Justification = "false positive")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA2210:AssembliesShouldHaveValidStrongNames", Justification = "Delay signed")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2243:AttributeStringLiteralsShouldParseCorrectly", Justification = "Version contains pre-release data")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "Owin", Justification = "By design")]
