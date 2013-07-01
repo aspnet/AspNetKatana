f83d38a1ce786494fe2a5d8b00633ec844dc46d7
Chris Ross <Tratcher@Outlook.com>
2013-07-01 09:59:47 -0700

    CodeAnalysis: Remove redundant supressions.

--- src/Katana.Boot.AspNet/GlobalSuppressions.cs
+++ src/Katana.Boot.AspNet/GlobalSuppressions.cs
@@ -47,4 +47,3 @@ using System.Diagnostics.CodeAnalysis;
 [assembly: SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode", Scope = "member", Target = "System.Threading.Tasks.CatchInfo.#Handled()", Justification = "Dependency included by source")]
 [assembly: SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode", Scope = "member", Target = "System.Threading.Tasks.CatchInfo.#Task(System.Threading.Tasks.Task)", Justification = "Dependency included by source")]
 [assembly: SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode", Scope = "member", Target = "System.Threading.Tasks.CatchInfo.#Throw(System.Exception)", Justification = "Dependency included by source")]
-[assembly: SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode", Scope = "member", Target = "Owin.StartupExtensions.#UseType`1(Owin.IAppBuilder,System.Object[])", Justification = "Dependency included by source")]
