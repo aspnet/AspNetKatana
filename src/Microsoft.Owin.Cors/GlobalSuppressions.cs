bc038ac8d40972a98b8231d21b37710d4dc9738e
Louis DeJardin <lodejard@microsoft.com>
2013-07-09 14:37:55 -0700

    Adding empty CORS projects

---
+++ src/Microsoft.Owin.Cors/GlobalSuppressions.cs
@@ -0,0 +1,3 @@
+// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Cors", Justification = "Technology name")]
---
+++ tests/Microsoft.Owin.Cors.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("Microsoft.Owin.Cors.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("657bf6a7-44d4-4db0-948c-55572a50a123")]
