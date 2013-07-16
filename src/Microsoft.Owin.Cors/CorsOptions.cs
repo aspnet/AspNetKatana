b45b880535e47227aecf6dafc3d1a7ada236401c
David Fowler <davidfowl@gmail.com>
2013-07-16 12:34:18 -0700

    Added a static AllowAll cors options.

--- src/Microsoft.Owin.Cors/CorsOptions.cs
+++ src/Microsoft.Owin.Cors/CorsOptions.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
 
+using System.Threading;
 using System.Web.Cors;
 
 namespace Microsoft.Owin.Cors
@@ -9,6 +10,28 @@ namespace Microsoft.Owin.Cors
     /// </summary>
     public class CorsOptions
     {
+        private static CorsPolicy _allowAll;
+
+        /// <summary>
+        /// A policy that allows all headers, all methods, any origin and supports credentials
+        /// </summary>
+        public static CorsPolicy AllowAll
+        {
+            get
+            {
+                return LazyInitializer.EnsureInitialized(ref _allowAll, () =>
+                {
+                    return new CorsPolicy
+                    {
+                        AllowAnyHeader = true,
+                        AllowAnyMethod = true,
+                        AllowAnyOrigin = true,
+                        SupportsCredentials = true
+                    };
+                });
+            }
+        }
+
         /// <summary>
         /// The cors policy to apply
         /// </summary>
