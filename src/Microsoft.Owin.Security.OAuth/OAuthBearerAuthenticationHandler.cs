b739000ef9541f28119c062dc6972227548a4e95
Louis DeJardin <lodejard@microsoft.com>
2013-08-13 14:55:11 -0700

    Removing de-duplication logic - AppendValues should be sufficient

--- src/Microsoft.Owin.Security.OAuth/OAuthBearerAuthenticationHandler.cs
+++ src/Microsoft.Owin.Security.OAuth/OAuthBearerAuthenticationHandler.cs
@@ -113,11 +113,7 @@ namespace Microsoft.Owin.Security.OAuth
 
             if (challenge != null)
             {
-                IList<string> currentChallenges = Response.Headers.GetValues("WWW-Authetncicate");
-                if (currentChallenges == null || !currentChallenges.Contains(_challenge))
-                {
-                    Response.Headers.AppendValues("WWW-Authenticate", _challenge);
-                }
+                Response.Headers.AppendValues("WWW-Authenticate", _challenge);
             }
 
             return Task.FromResult<object>(null);
