a834334165732c65264149d301e57ca1888a28d0
Chris Ross <Tratcher@Outlook.com>
2013-08-15 10:59:50 -0700

    Stylecop.

--- src/Microsoft.Owin.Cors/ICorsPolicyProvider.cs
+++ src/Microsoft.Owin.Cors/ICorsPolicyProvider.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
 using System.Web.Cors;
 
 namespace Microsoft.Owin.Cors
