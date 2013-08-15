24a35703b541b999fa8971a68e3f95ed7791ca8b
Louis DeJardin <lodejard@microsoft.com>
2013-08-15 16:12:01 -0700

    Updating headers, code formatting

--- src/Microsoft.Owin.Cors/CorsOptions.cs
+++ src/Microsoft.Owin.Cors/CorsOptions.cs
@@ -1,6 +1,5 @@
 ﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
 
-using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Cors;
 
