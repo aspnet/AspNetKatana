35d5b72a85b63a488103b02e424a965d50fefd23
Chris Ross <Tratcher@Outlook.com>
2013-08-15 15:50:12 -0700

    Build: Set up servicing attribute. Make strong name version different for dual compiled assemblies.

--- build/CommonAssemblyInfo.cs
+++ build/CommonAssemblyInfo.cs
@@ -20,7 +20,13 @@ using System.Reflection;
 [assembly: AssemblyProduct("Microsoft OWIN")]
 [assembly: AssemblyCopyright("\x00a9 Microsoft Corporation All rights reserved.")]
 [assembly: AssemblyTrademark("")]
+#if NET40 // For servicing the dual compiled projects need different strong name versions
+[assembly: AssemblyVersion("2.0.0.400")]
+#elif NET45
+[assembly: AssemblyVersion("2.0.0.450")]
+#else
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.20729.0")]
-[assembly: AssemblyInformationalVersion("2.0.0-rtw-20729-000-dev")]
-[assembly: AssemblyMetadata("Servicing", "true")]
+#endif
+[assembly: AssemblyFileVersion("2.0.20815.0")]
+[assembly: AssemblyInformationalVersion("2.0.0-rtw-20815-000-dev")]
+[assembly: AssemblyMetadata("Serviceable", "True")]
