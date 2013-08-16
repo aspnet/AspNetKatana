3e9c523f1737ada0387b19e717eb2e3a21df7f61
Chris Ross <Tratcher@Outlook.com>
2013-08-16 15:32:12 -0700

    Build: Revert servicing version changes.

--- build/CommonAssemblyInfo.cs
+++ build/CommonAssemblyInfo.cs
@@ -6,13 +6,7 @@ using System.Reflection;
 [assembly: AssemblyProduct("Microsoft OWIN")]
 [assembly: AssemblyCopyright("\x00a9 Microsoft Corporation All rights reserved.")]
 [assembly: AssemblyTrademark("")]
-#if NET40 // For servicing the dual compiled projects need different strong name versions
-[assembly: AssemblyVersion("2.0.0.400")]
-#elif NET45
-[assembly: AssemblyVersion("2.0.0.450")]
-#else
 [assembly: AssemblyVersion("2.0.0.0")]
-#endif
 [assembly: AssemblyFileVersion("2.0.20815.0")]
 [assembly: AssemblyInformationalVersion("2.0.0-rtw-20815-000-dev")]
 [assembly: AssemblyMetadata("Serviceable", "True")]
