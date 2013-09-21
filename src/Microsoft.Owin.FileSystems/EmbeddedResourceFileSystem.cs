c1f51303e14810de6abc18ccd0818c7378eed79d
Damian Hickey <dhickey@gmail.com>
2013-09-21 10:13:14 +0100

    Initial EmbeddedResourceFileSystem implementation and tests. Supports TryGetFileInfo, but doesn't support TryGetDirectoryContents.

---
+++ src/Microsoft.Owin.FileSystems/EmbeddedResourceFileSystem.cs
@@ -0,0 +1,123 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace Microsoft.Owin.FileSystems
+{
+    
+
+    /// <summary>
+    ///     Looks up files using embedded resources in the specified assembly
+    /// </summary>
+    public class EmbeddedResourceFileSystem : IFileSystem
+    {
+        private readonly Assembly _assembly;
+        private readonly string _baseNamespace;
+        private readonly DateTime _lastModified;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="EmbeddedResourceFileSystem" /> class using the calling
+        ///     assembly and empty base namespace.
+        /// </summary>
+        public EmbeddedResourceFileSystem()
+            : this(Assembly.GetCallingAssembly())
+        { }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="EmbeddedResourceFileSystem" /> class using the specified
+        ///     assembly and empty base namespace.
+        /// </summary>
+        /// <param name="assembly"></param>
+        public EmbeddedResourceFileSystem(Assembly assembly)
+            : this(assembly, string.Empty)
+        { }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="EmbeddedResourceFileSystem" /> class using the calling
+        ///     assembly and specified base namespace.
+        /// </summary>
+        /// <param name="baseNamespace">The base namespace that contains the embedded resources.</param>
+        public EmbeddedResourceFileSystem(string baseNamespace)
+            : this(Assembly.GetCallingAssembly(), baseNamespace)
+        { }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="EmbeddedResourceFileSystem" /> class using the specified
+        ///     assembly and root namespace.
+        /// </summary>
+        /// <param name="assembly">The assembly that contains the embedded resources.</param>
+        /// <param name="baseNamespace">The base namespace that contains the embedded resources.</param>
+        public EmbeddedResourceFileSystem(Assembly assembly, string baseNamespace)
+        {
+            if (assembly == null)
+            {
+                throw new ArgumentNullException("assembly");
+            }
+            _baseNamespace = baseNamespace ?? string.Empty;
+            _assembly = assembly;
+            _lastModified = new FileInfo(assembly.Location).LastWriteTime;
+        }
+
+        public bool TryGetFileInfo(string subpath, out IFileInfo fileInfo)
+        {
+            string resourcePath = string.Concat(_baseNamespace, ".", subpath.Replace("/", "."));
+            if (_assembly.GetManifestResourceInfo(resourcePath) == null)
+            {
+                fileInfo = null;
+                return false;
+            }
+            fileInfo = new EmbeddedResourceFileInfo(_assembly, resourcePath, _lastModified);
+            return true;
+        }
+
+        public bool TryGetDirectoryContents(string subpath, out IEnumerable<IFileInfo> contents)
+        {
+            throw new NotSupportedException("Directory functionality not supported.");
+        }
+
+        private class EmbeddedResourceFileInfo : IFileInfo
+        {
+            private readonly DateTime _lastModified;
+            private readonly Stream _resourceStream;
+
+            public EmbeddedResourceFileInfo(Assembly assembly, string resourcePath, DateTime lastModified)
+            {
+                _lastModified = lastModified;
+                _resourceStream = assembly.GetManifestResourceStream(resourcePath);
+            }
+
+            public Stream CreateReadStream()
+            {
+                return _resourceStream;
+            }
+
+            public long Length
+            {
+                get { return _resourceStream.Length; }
+            }
+
+            public string PhysicalPath
+            {
+                get { return null; } //TODO What should be returned here?
+            }
+
+            public string Name
+            {
+                get { return null; } //TODO What should be returned here?
+            }
+
+            public DateTime LastModified
+            {
+                get { return _lastModified; }
+            }
+
+            public bool IsDirectory
+            {
+                get { return false; }
+            }
+        }
+    }
+}
---
+++ tests/Microsoft.Owin.FileSystems.Tests/EmbeddedResourceFileSystemTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Shouldly;
+using Xunit;
+
+namespace Microsoft.Owin.FileSystems.Tests
+{
+    public class EmbeddedResourceFileSystemTests
+    {
+        [Fact]
+        public void When_TryGetFileInfo_and_resource_does_not_exist_then_should_not_get_file_info()
+        {
+            var provider = new EmbeddedResourceFileSystem("Microsoft.Owin.FileSystems.Tests.Resources");
+
+            IFileInfo fileInfo;
+            provider.TryGetFileInfo("DoesNotExist.Txt", out fileInfo).ShouldBe(false);
+
+            fileInfo.ShouldBe(null);
+        }
+
+        [Fact]
+        public void When_TryGetFileInfo_and_resource_exists_in_root_then_should_get_file_info()
+        {
+            var provider = new EmbeddedResourceFileSystem("Microsoft.Owin.FileSystems.Tests.Resources");
+
+            IFileInfo fileInfo;
+            provider.TryGetFileInfo("File.txt", out fileInfo).ShouldBe(true);
+
+            fileInfo.ShouldNotBe(null);
+            fileInfo.LastModified.ShouldNotBe(default(DateTime));
+            fileInfo.Length.ShouldBeGreaterThan(0);
+            fileInfo.IsDirectory.ShouldBe(false);;
+            fileInfo.PhysicalPath.ShouldBe(null);
+            //TODO fileInfo.Name.Should().Be(???)
+        }
+
+        [Fact]
+        public void When_TryGetFileInfo_and_resources_in_path_then_should_get_file_infos()
+        {
+            var provider = new EmbeddedResourceFileSystem("Microsoft.Owin.FileSystems.Tests.Resources");
+
+            IFileInfo fileInfo;
+            provider.TryGetFileInfo("File.txt", out fileInfo).ShouldBe(true);
+
+            fileInfo.ShouldNotBe(null);
+            fileInfo.LastModified.ShouldNotBe(default(DateTime));
+            fileInfo.Length.ShouldBeGreaterThan(0);
+            fileInfo.IsDirectory.ShouldBe(false);
+            fileInfo.PhysicalPath.ShouldBe(null);
+            //TODO fileInfo.Name.Should().Be(???)
+        }
+    }
+}
