c5f0023e8049dad8f8a37ee0f6206ce79c626c4c
Chris Ross <Tratcher@Outlook.com>
2013-10-10 11:04:46 -0700

    Testing: Update copyright headers.

--- src/Microsoft.Owin.Testing/TestServer.cs
+++ src/Microsoft.Owin.Testing/TestServer.cs
@@ -1,18 +1,4 @@
-﻿// <copyright file="TestServer.cs" company="Microsoft Open Technologies, Inc.">
-// Copyright 2011-2013 Microsoft Open Technologies, Inc. All rights reserved.
-// 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// 
-//     http://www.apache.org/licenses/LICENSE-2.0
-// 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-// </copyright>
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
 
 using System;
 using System.Collections.Generic;
@@ -21,10 +7,8 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Owin.Hosting;
 using Microsoft.Owin.Hosting.Engine;
-using Microsoft.Owin.Hosting.Loader;
 using Microsoft.Owin.Hosting.ServerFactory;
 using Microsoft.Owin.Hosting.Services;
-using Microsoft.Owin.Hosting.Utilities;
 using Owin;
 
 namespace Microsoft.Owin.Testing
