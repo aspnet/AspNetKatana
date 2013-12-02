c40e78d777f9d6ef391b7f3f784bb801f2997a31
Chris Ross <Tratcher@Outlook.com>
2013-12-02 15:21:44 -0800

    FileSystems: Fix copyright headers.

--- src/Microsoft.Owin.StaticFiles/GlobalSuppressions.cs
+++ src/Microsoft.Owin.StaticFiles/GlobalSuppressions.cs
@@ -1,4 +1,6 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage 
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+
+// This file is used by Code Analysis to maintain SuppressMessage
 // attributes that are applied to this project.
 // Project-level suppressions either have no target or are given 
 // a specific target and scoped to a namespace, type, member, etc.
