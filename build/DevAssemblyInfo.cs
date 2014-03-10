bbad5cab2c619fdb3f253c3f8a3c675716cf0632
Chris Ross <Tratcher@Outlook.com>
2014-03-10 10:59:04 -0700

    Build: Add Servicable attribute for prototype packages.

--- build/DevAssemblyInfo.cs
+++ build/DevAssemblyInfo.cs
@@ -9,3 +9,4 @@ using System.Reflection;
 [assembly: AssemblyVersion("0.29.0.0")]
 [assembly: AssemblyFileVersion("0.29.30203.0")]
 [assembly: AssemblyInformationalVersion("0.29.0-pre-30203-000")]
+[assembly: AssemblyMetadata("Serviceable", "True")]
