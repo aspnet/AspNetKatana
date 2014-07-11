26ed214cae619ed96503775feb7346bc54aabe3b
Chris Ross <Tratcher@Outlook.com>
2014-07-11 10:07:41 -0700

    Build: Increment version to rc3.

--- build/DevAssemblyInfo.cs
+++ build/DevAssemblyInfo.cs
@@ -7,6 +7,6 @@ using System.Reflection;
 [assembly: AssemblyCopyright("\x00a9 Microsoft Corporation All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyVersion("0.30.0.0")]
-[assembly: AssemblyFileVersion("0.30.30328.0")]
-[assembly: AssemblyInformationalVersion("0.30.0-pre-30328-000")]
+[assembly: AssemblyFileVersion("0.30.30711.0")]
+[assembly: AssemblyInformationalVersion("0.30.0-pre-30711-000")]
 [assembly: AssemblyMetadata("Serviceable", "True")]
