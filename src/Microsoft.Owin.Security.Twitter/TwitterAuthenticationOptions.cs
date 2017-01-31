b2d0f97386bf3d8ba47407dc1de7cbf0f1628a45
Chris R <Tratcher@outlook.com>
2017-01-31 10:48:49 -0800

    #427 Remove pinned twitter certs

--- src/Microsoft.Owin.Security.Twitter/TwitterAuthenticationOptions.cs
+++ src/Microsoft.Owin.Security.Twitter/TwitterAuthenticationOptions.cs
@@ -25,15 +25,6 @@ namespace Microsoft.Owin.Security.Twitter
             CallbackPath = new PathString("/signin-twitter");
             AuthenticationMode = AuthenticationMode.Passive;
             BackchannelTimeout = TimeSpan.FromSeconds(60);
-
-            // Twitter lists its valid Subject Key Identifiers at https://dev.twitter.com/docs/security/using-ssl
-            BackchannelCertificateValidator = new CertificateSubjectKeyIdentifierValidator(
-                new[]
-                {
-                    "A5EF0B11CEC04103A34A659048B21CE0572D7D47", // VeriSign Class 3 Secure Server CA - G2
-                    "0D445C165344C1827E1D20AB25F40163D8BE79A5", // VeriSign Class 3 Secure Server CA - G3
-                    "5F60CF619055DF8443148A602AB2F57AF44318EF", // Symantec Class 3 Secure Server CA - G4
-                });
         }
 
         /// <summary>
