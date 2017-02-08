c33569969e79afd9fb4ec2d6bdff877e376821b2
Chris R <Tratcher@outlook.com>
2017-02-08 14:35:14 -0800

    Change license headers to .NET Foundation

--- src/Microsoft.Owin/GlobalSuppressions.cs
+++ src/Microsoft.Owin/GlobalSuppressions.cs
@@ -1,4 +1,5 @@
-﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1724:TypeNamesShouldNotMatchNamespaces", Scope = "type", Target = "Microsoft.Owin.Builder.AppBuilder", Justification = "By design")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1711:IdentifiersShouldNotHaveIncorrectSuffix", Scope = "type", Target = "Microsoft.Owin.ResponseCookieCollection", Justification = "It is a collection")]
--- tests/Microsoft.Owin.Hosting.Tests/GlobalSuppressions.cs
+++ tests/Microsoft.Owin.Hosting.Tests/GlobalSuppressions.cs
@@ -1,4 +1,5 @@
-// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1063:ImplementIDisposableCorrectly", Scope = "type", Target = "TinyIoC.SafeDictionary`2", Justification = "Dependency as source")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1063:ImplementIDisposableCorrectly", Scope = "member", Target = "TinyIoC.SafeDictionary`2.#Dispose()", Justification = "Dependency as source")]
