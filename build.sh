8e0c16f199955bc02a706f57b7a6e3264061c78a
Chris R <Tratcher@Outlook.com>
2017-02-17 09:58:46 -0800

    Delete unused build.sh file

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-export EnableNuGetPackageRestore=true
-mono ".nuget/NuGet.exe" install Sake -pre -o packages
-mono $(find packages/Sake.*/tools/Sake.exe|sort -r|head -n1) -f Sakefile.shade -I src/build "$@"
-
