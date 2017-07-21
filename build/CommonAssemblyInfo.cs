a4a61a67d1eb2231fa3212a6c5cb476dadef4e5b
Sijun Liu <sijliu@microsoft.com>
2017-07-21 11:09:33 -0700

    update wilson version

--- build/CommonAssemblyInfo.cs
+++ build/CommonAssemblyInfo.cs
@@ -8,6 +8,6 @@ using System.Reflection;
 [assembly: AssemblyCopyright("\x00a9 Microsoft Corporation All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyVersion("4.0.5.0")]
-[assembly: AssemblyFileVersion("4.0.60720.0")]
-[assembly: AssemblyInformationalVersion("4.0.5-alpha1-60720-000")]
+[assembly: AssemblyFileVersion("4.0.60721.0")]
+[assembly: AssemblyInformationalVersion("4.0.5-alpha1-60721-000")]
 [assembly: AssemblyMetadata("Serviceable", "True")]
--- tests/FunctionalTests/Properties/AssemblyInfo.cs
+++ tests/FunctionalTests/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("0.31.2.0")]
 [assembly: AssemblyVersion("0.31.2.0")]
-[assembly: AssemblyFileVersion("0.31.60720.0")]
+[assembly: AssemblyFileVersion("0.31.60721.0")]
