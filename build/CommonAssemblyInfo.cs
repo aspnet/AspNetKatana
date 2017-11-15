1653abe19b8740977e368ee0782f1e3eb2c65d03
Chris Ross <tratcher@outlook.com>
2017-11-15 15:47:15 -0800

    Drop the date from the package prerelease version due to length limits

--- build/CommonAssemblyInfo.cs
+++ build/CommonAssemblyInfo.cs
@@ -8,6 +8,6 @@ using System.Reflection;
 [assembly: AssemblyCopyright("\x00a9 Microsoft Corporation All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.0.61108.0")]
-[assembly: AssemblyInformationalVersion("4.0.0-preview1-61108-000")]
+[assembly: AssemblyFileVersion("4.0.61115.0")]
+[assembly: AssemblyInformationalVersion("4.0.0-preview1-000")]
 [assembly: AssemblyMetadata("Serviceable", "True")]
