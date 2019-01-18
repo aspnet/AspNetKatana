d63e1435934c1f8aff68cde0dc4a3f3e6d8085b5
Chris Ross <tratcher@outlook.com>
2019-01-18 10:07:26 -0800

    Bump patch version to 4.0.1

--- build/DevAssemblyInfo.cs
+++ build/DevAssemblyInfo.cs
@@ -8,6 +8,6 @@ using System.Reflection;
 [assembly: AssemblyCopyright("\x00a9 Microsoft Corporation All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyVersion("0.31.0.0")]
-[assembly: AssemblyFileVersion("0.31.61115.0")]
+[assembly: AssemblyFileVersion("0.31.80118.0")]
 [assembly: AssemblyInformationalVersion("0.31.0-pre-000")]
 [assembly: AssemblyMetadata("Serviceable", "True")]
--- tests/FunctionalTests/Properties/AssemblyInfo.cs
+++ tests/FunctionalTests/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("0.31.0.0")]
 [assembly: AssemblyVersion("0.31.0.0")]
-[assembly: AssemblyFileVersion("0.31.61115.0")]
+[assembly: AssemblyFileVersion("0.31.80118.0")]
