9134964332ee276461aa3e19e17c8a36fe65cd88
Chris Ross <tratcher@outlook.com>
2022-02-25 10:58:17 -0800

    Add build scripts

--- build.cmd
+++ build.cmd
@@ -1,11 +1,2 @@
 @echo off
-cd %~dp0
-
-IF EXIST .nuget\NuGet.exe goto part2
-echo Downloading latest version of NuGet.exe...
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command ".\build\downloadnuget.ps1"
-
-:part2
-set EnableNuGetPackageRestore=true
-.nuget\NuGet.exe install Sake -version 0.2 -o packages -source https://api.nuget.org/v3/index.json
-packages\Sake.0.2\tools\Sake.exe -I build -f Sakefile.shade %*
+powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0eng\common\Build.ps1""" -restore -build %*"
