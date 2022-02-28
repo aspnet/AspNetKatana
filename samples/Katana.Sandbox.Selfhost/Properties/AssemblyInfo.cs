b15381e6144fbc19864a43d52c4d53eb94dfe72d
Chris Ross <tratcher@outlook.com>
2022-02-28 10:24:10 -0800

    Convery more projects

--- samples/Katana.Sandbox.Selfhost/Properties/AssemblyInfo.cs
+++ samples/Katana.Sandbox.Selfhost/Properties/AssemblyInfo.cs
@@ -22,15 +22,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("9f2f31ef-6017-48cc-88d6-ec4ff4feaaa5")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.31.0.0")]
-[assembly: AssemblyVersion("0.31.0.0")]
-[assembly: AssemblyFileVersion("0.31.81112.0")]
